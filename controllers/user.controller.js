2e2b0c8e293b1f665a36d736b2d0f55569edc7d8
Himanshu Priyadarshi <24je0899@iitism.ac.in>
2025-07-06 12:21:04 +0530

    auth and logout

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,7 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const generateAccessAndRefreshToken=async (userId)=>{
   try {
@@ -83,8 +84,7 @@ export const login=asyncHandler(async(req,res)=>{
 
   const options={
     httpOnly:true,
-    secure:true,
-    sameSite:'strict'
+    secure:true
   }
 
   return res
@@ -101,5 +101,30 @@ export const login=asyncHandler(async(req,res)=>{
       },
       "User login succesfull"
     )
+  )
+})
+
+export const logout = asyncHandler(async(_ , res)=>{
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset:{
+        refreshToken:1
+      }
+    },
+    {
+      new:true
+    }
+  )
+  const options={
+    httpOnly:true,
+    secure:true
+  }
+  return res
+  .status(200)
+  .clearCookie("accessToken",options)
+  .clearCookie("refreshToken",options)
+  .json(
+    new ApiResponse(200,{},"Logout Successfully")
   )
 })
