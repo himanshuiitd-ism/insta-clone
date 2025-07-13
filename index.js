dde4e069bd5ee5f0faa76a63634bc7ed8e225d12
Himanshu Priyadarshi <24je0899@iitism.ac.in>
2025-07-13 22:23:08 +0530

    13/07/22:22

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ app.get("/",(req,res)=>{
 app.use(express.json())
 app.use(cookieParser())
 app.use(urlencoded({extended:true}));
-const corsOptions={
-  origin:'http://localhost:5173',
-  credentials:true
-}
-app.use(cors(corsOptions))
+
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow your frontend origin
+    credentials: true // Allow credentials
+  }));
 
 
 
