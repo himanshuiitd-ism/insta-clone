854d438b20f5a776d96eb546b3b841052b9b669e
Himanshu Priyadarshi <24je0899@iitism.ac.in>
2025-08-01 16:36:12 +0530

    deleted server.js and updated start command

--- sever.js
+++
@@ -1,8 +0,0 @@
-import("./backend/index.js")
-  .then(() => {
-    console.log("Server started successfully");
-  })
-  .catch((error) => {
-    console.error("Failed to start server:", error);
-    process.exit(1);
-  });
